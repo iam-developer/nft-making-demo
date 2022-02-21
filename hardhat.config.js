07161db16b1009555f62a0a34cf143df7747d3a4
sdev <sparmeet204@gmail.com>
2022-02-21 18:57:59 +0530

    Adding static data

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,12 @@
 /**
 * @type import('hardhat/config').HardhatUserConfig
 */
-
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("./scripts/deploy.js");
 require("./scripts/mint.js");
 
-const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+// const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
 
 module.exports = {
    solidity: "0.8.1",
@@ -15,13 +14,13 @@ module.exports = {
    networks: {
     hardhat: {},
     rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
-      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${'daEDcVO3uAY1AOh0U5RS6_Q7V8aYh7OT'}`,
+      accounts: [`0x${'8e9866a79a786878dfe09bb564345d29024878918126c1348e4453160c71b4f8'}`]
     },
     ethereum: {
       chainId: 1,
-      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
-      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+      url: `https://eth-mainnet.alchemyapi.io/v2/${'daEDcVO3uAY1AOh0U5RS6_Q7V8aYh7OT'}`,
+      accounts: [`0x${'8e9866a79a786878dfe09bb564345d29024878918126c1348e4453160c71b4f8'}`]
     },
   },
 }
--- scripts/helpers.js
+++ scripts/helpers.js
@@ -16,13 +16,13 @@ function getEnvVariable(key, defaultValue) {
 // Helper method for fetching a connection provider to the Ethereum network
 function getProvider() {
     return ethers.getDefaultProvider(getEnvVariable("NETWORK", "rinkeby"), {
-        alchemy: getEnvVariable("ALCHEMY_KEY"),
+        alchemy: 'daEDcVO3uAY1AOh0U5RS6_Q7V8aYh7OT',
     });
 }
 
 // Helper method for fetching a wallet account using an environment variable for the PK
 function getAccount() {
-    return new ethers.Wallet(getEnvVariable("ACCOUNT_PRIVATE_KEY"), getProvider());
+    return new ethers.Wallet('8e9866a79a786878dfe09bb564345d29024878918126c1348e4453160c71b4f8', getProvider());
 }
 
 // Helper method for fetching a contract instance at a given address
